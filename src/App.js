92bb2362008848cd943a33d9700600be81253db6
Abhivyaktii <abhivyaktismail@gmail.com>
2023-01-12 18:07:46 +0530

    adding firebase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from "react";
+import { async } from "q";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
 
 function App() {
+  const cart= useSelector(state=>state.cart)
   const isLoggedIn= useSelector((state=>state.auth.isLoggedIn))
-  console.log(isLoggedIn);
+  useEffect(() => {
+    const sendRequest= async()=>{
+      const res= await fetch('https://redux-store-cd7c3-default-rtdb.firebaseio.com//cartItems.json',{
+        method:"PUT",
+        body:JSON.stringify(cart)
+      })
+      const data= await res.json()
+      console.log("data ",data);
+    }
+    sendRequest()
+    
+  }, [cart])
+  
   return (
     <div className="App">
       {!isLoggedIn && <Auth />}
