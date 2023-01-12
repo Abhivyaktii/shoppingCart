794ba2a9117bf2666006ce23e47c2d3b50122eaf
Abhivyaktii <abhivyaktismail@gmail.com>
2023-01-12 12:34:10 +0530

    adding dispatch for add to cart

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./auth-slice";
+import cartSLice from "./cart-slice";
 const store=configureStore({
     reducer:{
-        auth:authSlice.reducer
+        auth:authSlice.reducer,
+        cart:cartSLice.reducer
     }
 })
 
